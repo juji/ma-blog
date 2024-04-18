fd5ac596edb5a5d8a8a5729424889e4aa999f66a
juji <jujiyangasli@gmail.com>
2024-04-19 00:29:28 +0700

    footer color

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -1,10 +1,27 @@
+
+
 import styles from './footer.module.css'
+
+const bg = [
+  '#fdbe02',
+  '#88fd02',
+  '#e902fd',
+  '#02fdf9',
+  '#fdf502',
+  '#02fd41',
+  '#fdbe02',
+  '#88fd02',
+  '#e902fd',
+  '#02fdf9',
+  '#fdf502',
+  '#02fd41',
+]
 
 export default function Footer({ className }: React.HTMLAttributes<HTMLElement>){
 
   return <footer className={`${styles.footer} ${className||''}`}>
     <div className={styles.footerSpacer}></div>
-    <div className={styles.footerContentContainer}>
+    <div className={styles.footerContentContainer} style={{background: bg[Math.floor(Math.random()*bg.length)]}}>
       <div className={styles.footerContent}>
         <div>footer left</div>
         <div>footer right</div>
--- src/components/post-page/index.tsx
+++ src/components/post-page/index.tsx
@@ -1,14 +1,18 @@
 import parse from 'html-react-parser';
 import type Post from "@/types/post";
 import styles from './post-page.module.css'
+import { format } from 'date-fns'
 
 export default function PostPage({ post }:{post:Post}){
 
   return <article className={styles.post}>
-    <h1>{post.title}</h1>
     {post.feature_image ? <div className={styles.postImage}>
       <img src={post.feature_image} alt={post.title} />
     </div> : null}
+    <div className={styles.header}>
+      <h1>{post.title}</h1>
+      <span>{format(post.published_at, 'PPP')}</span>
+    </div>
     <div>{parse(post.html)}</div>
   </article>
 
