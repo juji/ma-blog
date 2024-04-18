74d29829478fadfe7f85d5abddcd03ae7161e684
juji <jujiyangasli@gmail.com>
2024-04-18 10:49:31 +0700

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
+import "./reset.css";
 import "./globals.css";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
