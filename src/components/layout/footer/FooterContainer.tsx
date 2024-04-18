bfe8a762ca4c1b45e9091eb3e6b581618dd24edb
juji <jujiyangasli@gmail.com>
2024-04-19 01:14:16 +0700

    footer color: client rendering? 7

--- src/components/layout/footer/FooterContainer.tsx
+++ src/components/layout/footer/FooterContainer.tsx
@@ -2,36 +2,44 @@
 
 import { useMemo } from 'react'
 import { usePathname } from 'next/navigation'
+import { useRef } from 'react'
 
 // repetition makes it more random
 const bg = [
   '#fdbe02',
   '#88fd02',
-  '#ed47fc',
+  '#ef9af7',
   '#02fdf9',
   '#fdf502',
   '#02fd41',
   //
   '#fdbe02',
   '#88ff00',
-  '#ed47fc',
+  '#ef9af7',
   '#02fdf9',
   '#fdf502',
   '#02fd41',
   //
   '#fdbe02',
   '#88ff00',
-  '#ed47fc',
+  '#ef9af7',
   '#02fdf9',
   '#fdf502',
   '#02fd41',
 ]
 
+function getColor(last: string|null) : string {
+  const color = bg[Math.floor(Math.random()*bg.length)]
+  return last && last === color ? getColor(last) : color
+}
+
 export default function Footer({ className, children }: React.HTMLAttributes<HTMLElement>){
 
   const pathname = usePathname()
+  const lastColor = useRef<string|null>(null)
   const color = useMemo(() => {
-    return bg[Math.floor(Math.random()*bg.length)]
+    const col = getColor(lastColor.current)
+    return col
   },[ pathname ])
 
   return <div 
--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -1,4 +1,5 @@
 
+import Link from 'next/link'
 import styles from './footer.module.css'
 import FooterContainer from './FooterContainer'
 
@@ -8,8 +9,19 @@ export default function Footer({ className }: React.HTMLAttributes<HTMLElement>)
     <div className={styles.footerSpacer}></div>
     <FooterContainer className={styles.footerContentContainer}>
       <div className={styles.footerContent}>
-        <div>footer left</div>
-        <div>footer right</div>
+        <div className={styles.footerLeft}>
+          <p>Made by:</p>
+          <Link className={styles.jujiyangasli}
+            href="https://jujiyangasli.com" target="_blank" rel="noreferer noopener">
+            jujiyangasli.com
+          </Link>
+          <p>With{' '}
+            <Link href="https://nextjs.org/" 
+              target="_blank" rel="noreferer noopener">Next.js</Link> and{' '}
+            <Link href="https://ghost.org/" 
+              target="_blank" rel="noreferer noopener">Ghost</Link></p>
+        </div>
+        <div className={styles.footerRight}>@ 2024</div>
       </div>
     </FooterContainer>
     {/* <div className={styles.footerAd}>Ad</div> */}
