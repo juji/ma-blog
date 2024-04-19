578cfec0548fff63298019b57d0cac218ca232bd
juji <jujiyangasli@gmail.com>
2024-04-19 19:39:34 +0700

    revalidate number

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -39,7 +39,7 @@ export default async function Post({ params }: Props) {
     (process.env.GHOST_URL as string) +
     `/ghost/api/content/posts/slug/${params.slug}?key=` +
     (process.env.GHOST_KEY as string),
-    { next: { revalidate: 0 } }
+    { next: { revalidate: 900 } }
   ).then(res => res.json())
 
   return (
