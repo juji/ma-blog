d774bb91b96c5c6266f4596b1b40da7b40577a17
juji <jujiyangasli@gmail.com>
2024-04-20 02:55:25 +0700

    share buttons 3

--- src/components/share/isWebShareCompatible.ts
+++ src/components/share/isWebShareCompatible.ts
@@ -5,6 +5,10 @@ function isDektopSafari(){
   return /Safari/i.test(uA) && /Apple Computer/.test(vendor) && !/Mobi|Android/i.test(uA)
 }
 
-export default function isWebShareCompatible(){
-  return !isDektopSafari() && navigator?.canShare && navigator?.canShare()
+export default function isWebShareCompatible(data: {
+  title: string,
+  text: string,
+  url: string
+}){
+  return !isDektopSafari() && navigator?.canShare && navigator?.canShare(data)
 }
