b999f7fd71e7bfc14072a60f7d93341f426aa59c
juji <jujiyangasli@gmail.com>
2024-04-21 07:45:05 +0700

    sitemap update

---
+++ src/app/sitemap.multiple.ts
@@ -0,0 +1,42 @@
+
+import type { MetadataRoute } from 'next'
+// import type Post from '@/types/post'
+import type { Entry } from "contentful";
+import { BASE_URL } from '@/lib/constants'
+import { getPageNum, getPages } from '@/lib/content/contentful/sitemap'
+ 
+
+export async function generateSitemaps() {
+
+  // Fetch the total number of posts 
+  // and calculate the number of sitemaps needed
+  const num = await getPageNum()
+  return [ ...new Array(num) ].map((_,i) => ({ id: i }))
+
+}
+ 
+export default async function sitemap({
+  id,
+}: {
+  id: number
+}): Promise<MetadataRoute.Sitemap> {
+
+  const data = await getPages(id)
+
+  const dat = data.items.map((post:Entry) => ({
+    url: `${BASE_URL}/post/${post.fields.slug}`,
+    lastModified: new Date(post.sys.updatedAt),
+    changeFrequency: "daily",
+    priority: 0.8
+  }))
+
+  if(!id) dat.unshift({
+    url: `${BASE_URL}`,
+    lastModified: new Date(),
+    changeFrequency: "daily",
+    priority: 1
+  })
+
+  // @ts-ignore
+  return dat
+}
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,36 +1,19 @@
-
-import type { MetadataRoute } from 'next'
-// import type Post from '@/types/post'
+import { MetadataRoute } from 'next'
+import { getPages } from '@/lib/content/contentful/sitemap'
 import type { Entry } from "contentful";
 import { BASE_URL } from '@/lib/constants'
-import { getPageNum, getPages } from '@/lib/content/contentful/sitemap'
- 
 
-export async function generateSitemaps() {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
-  // Fetch the total number of posts 
-  // and calculate the number of sitemaps needed
-  const num = await getPageNum()
-  return [ ...new Array(num) ].map((_,i) => ({ id: i }))
-
-}
- 
-export default async function sitemap({
-  id,
-}: {
-  id: number
-}): Promise<MetadataRoute.Sitemap> {
-
-  const data = await getPages(id)
-
-  const dat = data.items.map((post:Entry) => ({
+  const data = await getPages(0)
+  const pages = data.items.map((post:Entry) => ({
     url: `${BASE_URL}/post/${post.fields.slug}`,
     lastModified: new Date(post.sys.updatedAt),
     changeFrequency: "daily",
     priority: 0.8
   }))
 
-  if(!id) dat.unshift({
+  pages.unshift({
     url: `${BASE_URL}`,
     lastModified: new Date(),
     changeFrequency: "daily",
@@ -38,5 +21,6 @@ export default async function sitemap({
   })
 
   // @ts-ignore
-  return dat
+  return pages
+  
 }
