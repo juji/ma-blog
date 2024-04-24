7070ea0cf40a405531d33829096cc0e2208c460c
juji <jujiyangasli@gmail.com>
2024-04-24 22:25:59 +0700

    test: draft

---
+++ src/app/draft/page.tsx
@@ -0,0 +1,31 @@
+
+import type { Metadata } from 'next'
+
+// import Post from '@/types/post';
+import Hero from "@/components/hero";
+import PostList from "@/components/post-list";
+import { getDrafts } from '@/lib/content/contentful/home';
+
+// import CodeHighlight from '@/components/code-highlight';
+
+export const metadata: Metadata = {
+  title: "Draft | Juji's Blog",
+  description: 'Just some notes that will help me with web development. I hope it can help you too. ;)',
+  openGraph: {
+    images: [ 'https://jujiyangasli.com/images/juji-1200-630.jpeg' ]
+  }
+}
+
+export default async function Home() {
+
+  const data = await getDrafts()
+
+  return (
+    <main>
+      <h1>Drafts</h1>
+      <PostList posts={data.items} />
+      {/* <CodeHighlight lang="json">{JSON.stringify(data,null,2)}</CodeHighlight>  */}
+    </main>
+  );
+}
+
--- src/lib/content/contentful/home.ts
+++ src/lib/content/contentful/home.ts
@@ -1,8 +1,19 @@
-import { getDeliveryClient } from './client'
+import { getDeliveryClient, getPreviewClient } from './client'
 
 export default async function getHome(skip = 0, limit = 10){
 
   return getDeliveryClient().getEntries({
+    skip,
+    limit,
+    // @ts-ignore
+    order: "-sys.createdAt"
+  })
+
+}
+
+export async function getDrafts(skip = 0, limit = 10){
+  
+  return getPreviewClient().getEntries({
     skip,
     limit,
     // @ts-ignore
