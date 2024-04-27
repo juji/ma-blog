1398e226da45d09436e027f10a6950c53d98c0f0
juji <jujiyangasli@gmail.com>
2024-04-27 08:01:38 +0700

    gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,11 +26,13 @@ export default function RootLayout({
       <head>
 
         {/* <!-- Google Tag Manager --> */}
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-WXVPD642');</script>
+        <script dangerouslySetInnerHTML={{ __html : `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-WXVPD642');
+        `}}></script>
         {/* <!-- End Google Tag Manager --> */}
 
         {/* https://nextjs.org/docs/messages/react-hydration-error */}
@@ -43,7 +45,7 @@ export default function RootLayout({
 
       {/* <!-- Google Tag Manager (noscript) --> */}
       <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WXVPD642"
-      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+      height="0" width="0" style={{display:'none', visibility:'hidden'}}></iframe></noscript>
       {/* <!-- End Google Tag Manager (noscript) --> */}
 
         <Header className="juji-header"/>
